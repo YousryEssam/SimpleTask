2d8e797c90c6d37d1f779ac7e8b89198b53b37ef
YousryEssam <youssry.essam2111@gmail.com>
2025-05-14 15:15:50 +0300

    feat: Implement header component with custom styles and navigation links
    feat: Create home component with hero section, features, and call to action
    
    feat: Add not found component with custom error styles and animations
    
    feat: Develop product details component with structured layout and specifications
    
    feat: Create product form component for adding new products with validation
    
    feat: Implement products component with table view, search functionality, and pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { ProductsComponent } from "./pages/products/products.component";
+import { ProductFormComponent } from "./pages/product-form/product-form.component";
+import { ProductDetailsComponent } from "./pages/product-details/product-details.component";
+import { NotFoundComponent } from "./pages/not-found/not-found.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    HeaderComponent, 
+    HomeComponent, 
+    ProductsComponent, 
+    ProductFormComponent, 
+    ProductDetailsComponent, 
+    NotFoundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
+import { ProductsComponent } from './pages/products/products.component';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
+import { ProductFormComponent } from './pages/product-form/product-form.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'Home',
+        pathMatch: 'full',
+    },
+    {
+        path: 'Home',
+        component: HomeComponent,
+    },
+    {
+        path: 'Products',
+        component: ProductsComponent,
+    },
+    {
+        path: 'Products/:id',
+        component: ProductDetailsComponent,
+    },
+    {
+        path: 'Products/:id/Edit',
+        component: ProductFormComponent,
+    },
+        {
+        path: '**',
+        component: NotFoundComponent,
+    },
+];
